3ccda3ceb670aa62d20988971287dd89da47ecbc
Rabindra Dahal <rabindradahal2076@gmail.com>
2020-02-11 21:21:24 +0545

    Added pagination in the site

--- Olx/Product/views.py
+++ Olx/Product/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
-from .models import Product
+from django.core.paginator import Paginator
+from .models import Product, ProductImages
 
 # Create your views here.
 def productlist(request):
     '''Product List Views'''
-    productlist = Product.objects.all()
+    product_lists = Product.objects.all()
     template = 'Product/product_list.html'
-    context = {"product_list":productlist}
+    # Code for pagination starts
+    paginator = Paginator(product_lists, 2)
+    page = request.GET.get('page')
+    product_lists = paginator.get_page(page)
+    # End of pagination
+    context = {"product_list":product_lists}
     return render(request, template, context)
 
 def productdetail(request, product_slug):
     '''Product Details Views'''
-    productdetail = Product.objects.get(slug=product_slug)
+    product_details = Product.objects.get(slug=product_slug)
+    product_images = ProductImages.objects.filter(product=product_details)
     template = 'Product/product_detail.html'
-    context = {"product_detail":productdetail}
+    context = {"product_detail":product_details, 'product_images':product_images}
     return render(request, template, context)
